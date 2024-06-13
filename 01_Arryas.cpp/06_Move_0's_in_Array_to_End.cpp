881b9efdcac8aa464e63f2b2b2630107e419edd8
Atul Mahalan <103631775+atulmahlan@users.noreply.github.com>
2024-06-13 10:52:58 +0530

    Update 06_Move_0's_in_Array_to_End.cpp

--- 01_Arryas.cpp/06_Move_0's_in_Array_to_End.cpp
+++ 01_Arryas.cpp/06_Move_0's_in_Array_to_End.cpp
@@ -1,5 +1,6 @@
 // QUESTION: Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
+// SOLUTION 1---->
 vector<int> swap(vector<int>& arr, int index1, int index2){
         int temp = arr[index1];
         arr[index1] = arr[index2];
@@ -25,5 +26,27 @@ vector<int> swap(vector<int>& arr, int index1, int index2){
                 swap(a,i,j);
                 j++;
             }
+        }
+    }
+
+
+// SOLUTION 2----->
+void moveZeroes(vector<int>& nums) 
+    {
+        // 1) Placement of non-zero
+        int k = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        // 2) Placement of 0s
+        for (int i = k; i < n; i++)
+        {
+            nums[i] = 0;
         }
     }
