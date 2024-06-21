bc38b87261a9537ec0f66c2b6b5f35f12ec83e9a
Atul Mahalan <103631775+atulmahlan@users.noreply.github.com>
2024-06-21 19:59:13 +0530

    Create 01_Sort_a_String.cpp

---
+++ 02_Strings/01_Sort_a_String.cpp
@@ -0,0 +1,20 @@
+string countSort(string str) {
+    vector<int> freq(26,0);
+    int n = str.length();
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        int index = i - 'a';
+        freq[index]++;
+    }
+
+    int j = 0;
+    for(int i = 0 ; i < 26 ; i++)
+    {
+        while(freq[i]--)
+        {
+            str[j++] = i + 'a';
+        }
+    }
+    return str;
+}
