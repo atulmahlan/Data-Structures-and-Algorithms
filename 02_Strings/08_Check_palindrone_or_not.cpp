7d509e09ea257551af557f081a00dfb5bd6aa685
Atul Mahalan <103631775+atulmahlan@users.noreply.github.com>
2024-06-25 04:29:44 +0530

    Update 08_Check_palindrone_or_not.cpp

--- 02_Strings/08_Check_palindrone_or_not.cpp
+++ 02_Strings/08_Check_palindrone_or_not.cpp
@@ -1,7 +1,7 @@
 // QUESTION: Given a string, check if it is palindrone or not.
 
 char toLowerCase(char ch) {
-    if(ch >= 'a' && ch <= 'z') return ch;
+    if((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')) return ch;
     else 
     {
         char temp = ch - 'A' + 'a';
