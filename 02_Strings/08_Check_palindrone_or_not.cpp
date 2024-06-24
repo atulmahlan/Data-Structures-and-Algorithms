5eed1e58ed7c7fe43ba4f8fbe1182ce45e0adae7
Atul Mahalan <103631775+atulmahlan@users.noreply.github.com>
2024-06-25 04:20:15 +0530

    Create 08_Check_palindrone_or_not.cpp

---
+++ 02_Strings/08_Check_palindrone_or_not.cpp
@@ -0,0 +1,30 @@
+// QUESTION: Given a string, check if it is palindrone or not.
+
+char toLowerCase(char ch) {
+    if(ch >= 'a' && ch <= 'z') return ch;
+    else 
+    {
+        char temp = ch - 'A' + 'a';
+        return temp;
+    }
+}
+
+bool isPalindrone(string s) {
+    int n = s.length();
+    int start = 0;
+    int end = n-1;
+    
+    while(start <= end)
+    {
+        if(toLowerCase(s[start]) != toLowerCase(s[end]))
+        {
+            return false;
+        }
+        else
+        {
+            start++;
+            end--;
+        }
+    }
+    return true;
+}
