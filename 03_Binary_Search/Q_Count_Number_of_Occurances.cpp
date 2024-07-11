3a6ac70bf4f1d6c4ed2d44649cf97cda17babbe0
Atul Mahalan <103631775+atulmahlan@users.noreply.github.com>
2024-07-11 10:42:00 +0530

    Create Q_Count_Number_of_Occurances.cpp

---
+++ 03_Binary_Search/Q_Count_Number_of_Occurances.cpp
@@ -0,0 +1,66 @@
+// QUESTION: Given a sorted array Arr of size N and a number X, you need to find the number of occurrences of X in Arr.
+
+
+
+int firstOccurrence(vector<int> &arr, int n, int k) {
+    int low = 0, high = n - 1;
+    int first = -1;
+
+    while (low <= high) 
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == k) 
+        {
+            first = mid;
+            high = mid - 1;
+        }
+        else if (arr[mid] < k) 
+        {
+            low = mid + 1;
+        }
+        else 
+        {
+            high = mid - 1;
+        }
+    }
+    return first;
+}
+
+int lastOccurrence(vector<int> &arr, int n, int k) 
+{
+    int low = 0, high = n - 1;
+    int last = -1;
+
+    while (low <= high) 
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == k) 
+        {
+            last = mid;
+            low = mid + 1;
+        }
+        else if (arr[mid] < k) 
+        {
+            low = mid + 1;
+        }
+        else 
+        {
+            high = mid - 1;
+        }
+    }
+    return last;
+}
+
+
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k) {
+    int first = firstOccurrence(arr, n, k);
+    if (first == -1) return { -1, -1};
+    int last = lastOccurrence(arr, n, k);
+    return {first, last};
+}
+
+int count(vector<int>& arr, int n, int x) {
+    pair<int, int> ans = firstAndLastPosition(arr, n, x);
+    if (ans.first == -1) return 0;
+    return (ans.second - ans.first + 1);
+}
