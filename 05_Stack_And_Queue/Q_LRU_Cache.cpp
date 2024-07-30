1892c98d8d07bd0fbf9275faea51b547543b75a8
Atul Mahalan <103631775+atulmahlan@users.noreply.github.com>
2024-07-30 19:39:56 +0530

    Create Q_LRU_Cache.cpp

---
+++ 05_Stack_And_Queue/Q_LRU_Cache.cpp
@@ -0,0 +1,89 @@
+// QUESTION: Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+// Implement the LRUCache class:
+// LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+// int get(int key) Return the value of the key if the key exists, otherwise return -1.
+// void put(int key, int value) Update the value of the key if the key exists. 
+// Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+// The functions get and put must each run in O(1) average time complexity.
+
+ 
+class LRUCache {
+private:
+    struct Node {
+        int key;
+        int value;
+        Node* prev;
+        Node* next;
+        Node(int k, int v) : key(k), value(v), prev(nullptr), next(nullptr) {}
+    };
+    
+    std::unordered_map<int, Node*> cache;
+    Node* head;
+    Node* tail;
+    int capacity;
+    int size;
+
+    void removeNode(Node* node) {
+        if (node->prev) node->prev->next = node->next;
+        if (node->next) node->next->prev = node->prev;
+        if (node == head) head = node->next;
+        if (node == tail) tail = node->prev;
+    }
+
+    void addToHead(Node* node) {
+        node->next = head;
+        node->prev = nullptr;
+        if (head) head->prev = node;
+        head = node;
+        if (!tail) tail = head;
+    }
+
+    void moveToHead(Node* node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    Node* removeTail() {
+        Node* node = tail;
+        if (tail->prev) tail->prev->next = nullptr;
+        tail = tail->prev;
+        if (!tail) head = nullptr;
+        return node;
+    }
+
+public:
+    LRUCache(int capacity) : capacity(capacity), size(0), head(nullptr), tail(nullptr) {}
+
+    int get(int key) {
+        if (cache.find(key) == cache.end()) return -1;
+        Node* node = cache[key];
+        moveToHead(node);
+        return node->value;
+    }
+
+    void put(int key, int value) {
+        if (cache.find(key) != cache.end()) {
+            Node* node = cache[key];
+            node->value = value;
+            moveToHead(node);
+        } else {
+            Node* newNode = new Node(key, value);
+            cache[key] = newNode;
+            addToHead(newNode);
+            size++;
+            if (size > capacity) {
+                Node* tailNode = removeTail();
+                cache.erase(tailNode->key);
+                delete tailNode;
+                size--;
+            }
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
